df2269854b275bea35f9c8723c60d7362af3448f
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2026-02-04 13:16:59 +0300

    refactor: change fetchProfile function

--- src/stores/profile.store.ts
+++ src/stores/profile.store.ts
@@ -2,13 +2,13 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 import type { Profile } from "@/interfaces/profile";
 import { API_ROUTES } from "@/api";
+import axios from "axios";
 
 export const useProfileStore = defineStore('profile', () => {
   const profile = ref<Profile>();
   async function fetchProfile()  {
-    const data = await fetch(API_ROUTES.profile);
-    const res = (await data.json()) as Profile
-    profile.value = res; 
+    const { data } = await axios.get<Profile>(API_ROUTES.profile)
+    profile.value = data; 
   }
   return { profile, fetchProfile };
 })
