7055e306e727a8aca8d35d0fc0cc49d0f4fee72a
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2026-02-04 14:36:56 +0300

    refactor: add vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+  routes: [],
+  history: createWebHistory(),
+})
 
 const pinia = createPinia();
 const app = createApp(App);
 app.use(pinia)
+app.use(router)
 app.mount('#app');
