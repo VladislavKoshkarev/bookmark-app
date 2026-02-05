047d2130c81c23906a941022139a676c66edb98e
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2026-02-05 13:17:45 +0300

    feat: add TextButton component

---
+++ src/components/TextButton.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <button class="button">
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+.button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: var(--color-fg);
+  color: var(--color-bg);
+  cursor: pointer;
+  border-radius: 50px;
+  border: none;
+  height: 50px;
+  padding: 12px 24px;
+  font-size: 18px;
+}
+.button :hover {
+  background: var(--color-fg-hover);
+}
+</style>
