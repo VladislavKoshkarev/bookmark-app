ae3cb4874189504c98ae210e8cce2957abb1b52f
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2026-02-05 13:26:46 +0300

    feat: add authentication page

---
+++ src/pages/AuthView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import ButtonText from '@/components/TextButton.vue';
+import { useRouter } from 'vue-router';
+
+const router = useRouter();
+
+function redirectToMain() {
+  router.push({
+    name: 'main'
+  })
+}
+</script>
+
+<template>
+  <div class="auth">
+    <h1 class="auth__header">Bookmarkly</h1 >
+    <ButtonText @click="redirectToMain">Вход</ButtonText>
+  </div>
+</template>
+
+<style scoped>
+  .auth {
+    height: 100vw;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 52px;
+  }
+  .auth__header {
+    font-size: 52px;
+    font-weight: 700;
+  }
+</style>
