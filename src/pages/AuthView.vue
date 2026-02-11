36e57cefc38a1b5531fc383b28cce5dc1a7f0bcc
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2026-02-11 13:17:24 +0300

    feat: add input to AuthView page

--- src/pages/AuthView.vue
+++ src/pages/AuthView.vue
@@ -1,34 +1,43 @@
 <script setup lang="ts">
+import InputString from '@/components/InputString.vue';
 import ButtonText from '@/components/TextButton.vue';
-import { useRouter } from 'vue-router';
+import { ref } from 'vue';
 
-const router = useRouter();
+const form = ref<{ email?: string, password?: string }>({});
 
-function redirectToMain() {
-  router.push({
-    name: 'main'
-  })
+function onSubmit(event: Event) {
+  event.preventDefault();
+  console.log(form.value)
+  form.value = {}
 }
 </script>
 
 <template>
   <div class="auth">
-    <h1 class="auth__header">Bookmarkly</h1 >
-    <ButtonText @click="redirectToMain">Вход</ButtonText>
+    <h1 class="auth__header">Bookmarkly</h1>
+    <form class="auth__form" @submit="onSubmit">
+      <InputString v-model="form.email" placeholder="Email"></InputString>
+      <InputString v-model="form.password" placeholder="Пароль" type="password"></InputString>
+      <ButtonText>Вход</ButtonText>
+    </form>
   </div>
 </template>
 
 <style scoped>
   .auth {
-    height: 100vw;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    gap: 52px;
+     display: grid;
+    place-items: center;
+    min-height: 50vh;
   }
   .auth__header {
     font-size: 52px;
     font-weight: 700;
   }
+  .auth__form {
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    gap: 52px;
+    display: flex;
+}
 </style>
