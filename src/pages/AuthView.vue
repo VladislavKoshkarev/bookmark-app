4e0a828cdecb9f6126cd9f8d13a868539a2ca57f
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2026-02-17 17:47:13 +0300

    refactor: add Clerk signUp form

--- src/pages/AuthView.vue
+++ src/pages/AuthView.vue
@@ -1,25 +1,11 @@
 <script setup lang="ts">
-import InputString from '@/components/InputString.vue';
-import ButtonText from '@/components/TextButton.vue';
-import { ref } from 'vue';
-
-const form = ref<{ email?: string, password?: string }>({});
-
-function onSubmit(event: Event) {
-  event.preventDefault();
-  console.log(form.value)
-  form.value = {}
-}
+import { SignUp } from '@clerk/vue';
 </script>
 
 <template>
   <div class="auth">
     <h1 class="auth__header">Bookmarkly</h1>
-    <form class="auth__form" @submit="onSubmit">
-      <InputString v-model="form.email" placeholder="Email"></InputString>
-      <InputString v-model="form.password" placeholder="Пароль" type="password"></InputString>
-      <ButtonText>Вход</ButtonText>
-    </form>
+    <SignUp forceRedirectUrl="/main"/>
   </div>
 </template>
 
