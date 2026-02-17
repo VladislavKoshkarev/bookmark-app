85363dd7cea200adc1c3ceacb1467cd948c0b447
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2026-02-17 17:44:05 +0300

    feat: add auth store

---
+++ src/stores/auth.store.ts
@@ -0,0 +1,10 @@
+import { useAuth } from "@clerk/vue";
+import { defineStore } from "pinia";
+
+export const useAuthStore = defineStore('auth', () => {
+  const { getToken } = useAuth()
+  async function getSessionToken() {
+    return await getToken.value()
+  }
+  return { getSessionToken }
+})
