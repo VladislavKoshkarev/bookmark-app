aaa809b6efc623f59b73cc7a015e3cbf11e9ebea
Vladislav Koshkarev <dddvvv.dv@gmail.com>
2026-03-09 16:14:29 +0300

    feat: add confirmation popup

--- src/components/BookmarkCard.vue
+++ src/components/BookmarkCard.vue
@@ -4,15 +4,30 @@ import type { Bookmark } from '@/interfaces/bookmark';
 import IconButtonBig from './IconButtonBig.vue';
 import LinkIconWhite from '@/icons/LinkIconWhite.vue';
 import { useBookmarksStore } from '@/stores/bookmarks.store';
+import PopupConfirm from './PopupConfirm.vue';
+import { ref } from 'vue';
 
 
 const { id, category_id, title, image, url } = defineProps<Bookmark>()
 
 const bookmarksStore = useBookmarksStore()
 
+const isOpened = ref<boolean>(false)
+
 function openLink() {
   window.open(url)
 }
+
+function toggleIsOpened() {
+  isOpened.value = !isOpened.value
+}
+
+function deleteBookmark() {
+  toggleIsOpened()
+  bookmarksStore.deleteBookmark(id, category_id)
+
+}
+
 </script>
 
 <template>
@@ -20,13 +35,19 @@ function openLink() {
      <div class="bookmark-card__image" :style="{backgroundImage: `url(${image })`}"></div>
      <div class="bookmark-card__title"> {{ title }}</div>
      <div class="bookmark-card__footer">
-      <IconButtonBig @click="bookmarksStore.deleteBookmark(id, category_id)">
+      <IconButtonBig @click="toggleIsOpened()">
         <TrashIconWhite />
       </IconButtonBig>
       <IconButtonBig @click="openLink">
         <LinkIconWhite />
       </IconButtonBig>
      </div>
+     <PopupConfirm 
+      text="Хотите удалить закладку?" 
+      :is-opened="isOpened"
+      @confirm='deleteBookmark'
+      @cancel='toggleIsOpened'
+      ></PopupConfirm>
   </div>
 </template>
 
---
+++ src/components/PopupConfirm.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import TextButton from './TextButton.vue';
+
+const { isOpened, text} = defineProps<{ 
+  isOpened: boolean,
+  text: string,
+}>()
+const emit = defineEmits<{
+  (e: 'confirm'): void,
+  (e: 'cancel'): void,
+}>()
+</script>
+
+<template>
+  <Teleport to="body">
+    <div class="popup__cover" v-if="isOpened">
+      <div class="popup">
+        {{ text }}
+        <div class="popup__actions">
+          <TextButton @click="emit('confirm')">Да</TextButton>
+          <TextButton @click="emit('cancel')">Нет</TextButton>
+        </div>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<style scoped>
+.popup__cover {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: var(--color-inactive);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.popup {
+  background-color: var(--color-bg);
+  padding: 20px;
+  border-radius: 20px;
+  min-width: 350px;
+}
+.popup__actions {
+  margin-top: 16px;
+  display: flex;
+  gap: 10px;
+}
+</style>
